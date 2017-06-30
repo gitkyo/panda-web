a85d66e15e79a7784ac671d23c4359b1651db4cc
gitkyo <p.delaunay1@gmail.com>
2017-06-30 12:31:23 +0200

    add  templates & swiper scripts

---
+++ modules/custom/hook_footer/src/Controller/hook_footer_Controller.php
@@ -0,0 +1,27 @@
+<?php 
+/**
+	@file
+	Contains \Drupal\hook_hp\Controller\hook_hp_Controller.
+*/
+
+/*
+Faire ici un hook hook_page_preprocess() : https://api.drupal.org/api/drupal/core!lib!Drupal!Core!Render!theme.api.php/group/themeable/8.2.x
+
+doc query :  http://www.eilyin.name/note/database-queries-drupal-8-7
+*/
+
+/*namespace Drupal\hook_hp\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class hook_hp_Controller extends ControllerBase {
+	
+	public function content() {		
+
+		$query = \Drupal::database()->select('file_managed', 'f');
+		drupal_set_message($message = 'NULL', $type = 'status', $repeat = FALSE);
+
+
+	}
+}*/
+
