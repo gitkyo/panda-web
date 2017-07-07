7a9c44aa0632a34ea7f8bdb50ca903de75d3edc6
HamoudaAmine <amine.hamouda@gmail.com>
2017-07-07 11:57:03 +0200

    responsive

--- themes/bootstrap_panda/scripts/script-hp.js
+++ themes/bootstrap_panda/scripts/script-hp.js
@@ -6,7 +6,9 @@ $('#mainNav').affix({
   offset: {
     top: 100
   }
+
 })
+
     //doc swiper : http://idangero.us/swiper/api/#.WVUUzIjyhaQ
     var startGallery = function(){
 
@@ -19,18 +21,24 @@ $('#mainNav').affix({
            nextButton: '.swiper-button-next',
            prevButton: '.swiper-button-prev',
 
-
             speed: 400,
             spaceBetween: 100
         });
     }
-
+var scrollToAncre = function(){
+		jQuery('a[href^="#"], button.ancre[href^="#"]').click(function(){  
+		    var id = jQuery(this).attr("href");
+		    var offset = jQuery(id).offset().top 
+		    jQuery('html, body').animate({scrollTop: offset}, 'slow'); 
+		    return false;  
+		});
+	}
 
 
     window.init = function() {
 
         startGallery();
-
+scrollToAncre ();
     }
 
     init(); // true
