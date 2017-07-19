dcf76bdab627464d30c8b5b7be521cd0645989fe
HamoudaAmine <amine.hamouda@gmail.com>
2017-07-19 10:15:17 +0200

    use media queries

--- themes/bootstrap_panda/scripts/script-hp.js
+++ themes/bootstrap_panda/scripts/script-hp.js
@@ -9,7 +9,7 @@ jQuery(document).ready(function () {
   var scrollToAncre = function () {
     jQuery('a[href^="#"][class*="smooth"], button.ancre[href^="#"]').click(function () {
       var id = jQuery(this).attr("href");
-      var offset = jQuery(id).offset().top
+      var offset = jQuery(id).offset().top;
       jQuery('html, body').animate({
         scrollTop: offset
       }, 'slow');
@@ -28,12 +28,44 @@ jQuery(document).ready(function () {
 	        autoplay : 2500,	        
 	        loop: true,
 	        spaceBetween: 120,
+          // Responsive breakpoints
+			breakpoints: {
+				// when window width is <= 320px
+				320: {
+				  slidesPerView: 1,
+				  spaceBetween: 10
+				},
+				// when window width is <= 480px
+				480: {
+				  slidesPerView: 2,
+				  spaceBetween: 15
+				},
+				// when window width is <= 640px
+				640: {
+				  slidesPerView: 3,
+				  spaceBetween: 25
+				},
+				// when window width is <= 768px
+				768: {
+				  slidesPerView: 4,
+				  spaceBetween: 25
+				},
+				// when window width is <= 992px
+				992: {
+				  slidesPerView: 5,
+				  spaceBetween: 25
+				},
+				// when window width is <= 992px
+				1200: {
+				  slidesPerView: 6,
+				  spaceBetween: 90
+				}
+			}
 		})
 	}
   window.init = function () {
     scrollToAncre();
     setSlider();
-    
   }
   init(); // true
 });
